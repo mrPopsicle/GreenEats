64f44dfa3dc432cbee1a96b0182a1e6f1dc6ee45
shawn <shawnsantos186@gmail.com>
2025-07-31 02:48:08 +0800

    User organized to package

--- src/main/java/com/example/greeneats/model/Cart.java
+++ src/main/java/com/example/greeneats/model/Cart.java
@@ -22,7 +22,13 @@ public class Cart {
     }
 
     public void removeItem(MenuItem item) {
-        items.removeIf(cartItem -> cartItem.getItem().getName().equals(item.getName()));
+        for (int i = 0; i < items.size(); i++) {
+            CartItem cartItem = items.get(i);
+            if (cartItem.getItem().getName().equals(item.getName())) {
+                items.remove(i);
+                break;
+            }
+        }
     }
 
     public void clearCart() {
